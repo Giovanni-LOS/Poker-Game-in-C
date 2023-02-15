4da80068b4dbabfcbefbe428d7c72d4aa3da4805
GigiLucas <gio.vannnni@hotmail.com>
2023-02-15 14:51:34 -0300

    Implementation of the hand valuation.

--- Poker.c
+++ Poker.c
@@ -4,9 +4,11 @@
 #define FACES 13
 #define SUITS 4
 #define CARDS 52
+#define HANDS 10
 
 void shuffle(unsigned int deckShuffle[][FACES]);
 void drawCardsToPlayer(int *histogram, int *player);
+void handValue(int *playerCards, int *playerHandValue, unsigned int deckValuation[][FACES]);
 
 int main()
 {
@@ -14,15 +16,21 @@ int main()
     srand(time(NULL));
     shuffle(deck);
     const char *suits[SUITS] = {"Hearts", "Diamond", "Clubs", "Spades"};
-    const char *faces[FACES] = {"Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+    const char *faces[FACES] = {"Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+    const char *hands[HANDS] = {"High Card", "Pair", "Two Pair's", "Three of a Kind", "Straight", "Flush", "Full House", "Four of a Kind", "Straight Flush", "Royal Flush"};
     int histogramOfDrawCards[CARDS] = {0};
-    int player1[5] = {0}, player2[5] = {0}, player3[5] = {0}, player4[5] = {0}, player5[5] = {0};
-    drawCardsToPlayer(histogramOfDrawCards, player1);
-    drawCardsToPlayer(histogramOfDrawCards, player2);
-    drawCardsToPlayer(histogramOfDrawCards, player3);
-    drawCardsToPlayer(histogramOfDrawCards, player4);
-    drawCardsToPlayer(histogramOfDrawCards, player5);
-
+    int player1Cards[5] = {0}, player2Cards[5] = {0}, player3Cards[5] = {0}, player4Cards[5] = {0}, player5Cards[5] = {0};
+    drawCardsToPlayer(histogramOfDrawCards, player1Cards);
+    drawCardsToPlayer(histogramOfDrawCards, player2Cards);
+    drawCardsToPlayer(histogramOfDrawCards, player3Cards);
+    drawCardsToPlayer(histogramOfDrawCards, player4Cards);
+    drawCardsToPlayer(histogramOfDrawCards, player5Cards);
+    int player1HandValue = 0, player2HandValue = 0, player3HandValue = 0, player4HandValue = 0, player5HandValue = 0;
+    handValue(player1Cards, &player1HandValue, deck);
+    handValue(player2Cards, &player2HandValue, deck);
+    handValue(player3Cards, &player3HandValue, deck);
+    handValue(player4Cards, &player4HandValue, deck);
+    handValue(player5Cards, &player5HandValue, deck);
     return 0;
 }
 
@@ -41,7 +49,7 @@ void shuffle(unsigned int deckShuffle[][FACES])
     }
 }
 
-void drawCardsToPlayer(int *histogram, int *player)
+void drawCardsToPlayer(int *histogram, int *playerCardsToDraw)
 {
     size_t card = rand() % CARDS;
     int cardsDrawed = 0;
@@ -49,10 +57,97 @@ void drawCardsToPlayer(int *histogram, int *player)
     {
         if (histogram[card] == 0)
         {
-            player[cardsDrawed] = card;
+            playerCardsToDraw[cardsDrawed] = card;
             histogram[card]++;
             cardsDrawed++;
         }
         card = rand() % CARDS;
+    }
+}
+
+void handValue(int *playerCards, int *playerHandValue, unsigned int deckValuation[][FACES])
+{
+    // Straight histograms to find the hand rankings.
+    int histogramFaces[FACES] = {0};
+    int histogramSuits[SUITS] = {0};
+    int playerCardsCounter = 0;
+    while (playerCardsCounter < 5)
+    {
+        for (size_t i = 0; i < SUITS; i++)
+        {
+            for (size_t j = 0; j < FACES; j++)
+            {
+                if (deckValuation[i][j] == playerCards[playerCardsCounter])
+                {
+                    histogramFaces[j]++;
+                    histogramSuits[i]++;
+                    playerCardsCounter++;
+                }
+            }
+        }
+    }
+    // Find a pair, 2 pair's, three of a kind, four of a kind and full houses.
+    // Setting the value to a variable who will become the index of the Hand.
+    for (size_t i = 0; i < FACES; i++)
+    {
+        if (histogramFaces[i] == 2)
+        {
+            if (*playerHandValue == 1)
+            {
+                *playerHandValue = 2;
+            }
+            if (*playerHandValue == 3)
+            {
+                *playerHandValue = 6;
+            }
+            *playerHandValue = 1;
+        }
+        if (histogramFaces[i] == 3)
+        {
+            if (*playerHandValue == 1)
+            {
+                *playerHandValue = 6;
+            }
+            *playerHandValue = 3;
+        }
+        if (histogramFaces[i] == 4)
+        {
+            *playerHandValue = 7;
+        }
+    }
+    // Find a straight, flush, straight flush and royal flush.
+    // Setting the value to a variable who will become the index of the Hand.
+    if (*playerHandValue == 0)
+    {
+        int sequence = 0;
+        for (size_t i = 0; i < FACES - 1; i++)
+        {
+            if (histogramFaces[i] == 1 && histogramFaces[i + 1] == 1)
+            {
+                sequence++;
+            }
+            if (sequence == 4)
+            {
+                *playerHandValue = 4;
+            }
+        }
+        for (size_t i = 0; i < SUITS; i++)
+        {
+            if (histogramSuits[i] == 5)
+            {
+                if (*playerHandValue == 4)
+                {
+                    *playerHandValue = 8;
+                    if (histogramFaces[12] == 1)
+                    {
+                        *playerHandValue = 9;
+                    }
+                }
+                else
+                {
+                    *playerHandValue = 5;
+                }
+            }
+        }
     }
 }
